e4352aa8dabe65e751500148f9a4b7163b024462
siwon398 <scaipwoen8@naver.com>
2024-07-08 19:02:57 +0900

    Update fan_controler.v

--- sources_1/new/fan_controler.v
+++ sources_1/new/fan_controler.v
@@ -1,231 +1,389 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////
-module counter_pwm(
-    input clk, reset_p,
-    input btn_pedge,
-    output reg [7:0] power,
-    output reg[2:0] led
-    );
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/06/15 15:22:10
+// Design Name: 
+// Module Name: multi_fan_cntr
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-    reg [1:0] cnt;
-    
-     always @ (posedge clk or posedge reset_p) begin
-        if(reset_p) begin
-            cnt = 0;
-            power = 0;
-            led =0;
-        end
-        else if (btn_pedge) begin                       
-            cnt = cnt + 1;
-            case(cnt)
-                2'b00 : begin
-                    power = 7'd0;
-                    led = 0;                    
-                end       
-                2'b01 : begin
-                   led =0;
-                    power = 7'd42;
-                    led[0] = 1;
-                end                                
-                2'b10 : begin
-                    led = 0;
-                    power = 7'd84;
-                    led[1] = 1;
-                end 
-                2'b11 : begin
-                    led =0;
-                    power = 7'd127;
-                    led[2] = 1;
-                end             
-                default  power = 7'd0 ;
-            endcase               
-        end           
-    end
-endmodule
-//////////////////////////////////////////////
-module pwm_128step_fan(
+///////////////////////ÏÑ†ÌíçÍ∏∞ ÌåÄÌîÑÎ°úÏ†ùÌä∏ ÏôÑÏÑ±/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+module led_brightness( //pull-up Ï†ÄÌï≠ÏúºÎ°ú ÌïòÏûê
     input clk, reset_p,
-    input [6:0] duty,
-    input [13:0]pwm_freq,
-    output reg pwm_128
-    );
-    parameter sys_clk_freq = 100_000_000; //125_000_000  
-    
-    reg[26:0] cnt;
-    reg pwm_freqX128;
-    
-    wire [26:0]temp; //100_000_000 ¿Ã¡¯ºˆ   
-   
-    integer cnt_sysclk;
-    assign temp = sys_clk_freq/pwm_freq;
+    input btn,
+    output reg led_pwm_o
+);
 
-    always @(posedge clk or posedge reset_p) begin
-        if(reset_p) begin
-            pwm_freqX128 = 0;
-            cnt = 0;
-        end
-        else begin
-            if(cnt >= temp[26:7] - 1) cnt = 0;//¿ﬂ∂Ûº≠ πˆ∏∞¥Ÿ == shiftø¨ªÍ,,, π›¥Î¥¬ 0 √ﬂ∞°
-            else cnt = cnt + 1;
-                
-            if(cnt <temp[26:8]) pwm_freqX128 = 0;
-            else pwm_freqX128 = 1;
-        end               
-    end
-   
-    wire pwm_freqX100_nedge;
-    edge_detector_n ed(.clk(clk), .reset_p(reset_p), .cp(pwm_freqX128),.n_edge(pwm_freqX100_nedge));
+    wire [1:0]led_pwm;
+    reg [27:0] clk_div = 0;
+    always@(posedge clk) clk_div =clk_div+1;
     
-    reg [6:0] cnt_duty;
-   
-    always @(posedge clk or posedge reset_p) begin
-        if(reset_p) begin
-            cnt_duty = 0;
-            pwm_128 = 0;            
-        end
-        else begin
-            if(pwm_freqX100_nedge) begin
-                                                   
-                cnt_duty = cnt_duty + 1;
-                
-                if(cnt_duty < duty) pwm_128 = 1;
-                else pwm_128 = 0;
-            end
+    pwm_100pc led0(.clk(clk), //1Îã®
+              .reset_p(reset_p),
+              .duty(50),
+              .pwm_freq(1_000_000),
+              .pwm_100pc(led_pwm[0])
+              );
               
-        end
-    end   
-endmodule   
-/////////////////////////////////////////////////////////////////////////////
-module loadable_downcounter_dec_60_fan(
-    input clk, reset_p,
-    input clk_time,
-    input load_enable,
-    input [3:0] set_value1,
-    output reg [3:0] dec1, dec10,
-    output reg dec_clk
-    );
+    pwm_100pc led1(.clk(clk), //2Îã®
+              .reset_p(reset_p),
+              .duty(90),
+              .pwm_freq(1_000_000),
+              .pwm_100pc(led_pwm[1])
+              );
+    
+    wire btn_pedge;        
+    button_cntr btn_cntr0(.clk(clk), .reset_p(reset_p), .btn(btn), .btn_pe(btn_pedge));         
+            
+    reg [1:0]cnt_btn; //0,1,2,3 -> Í∫ºÏßê, 1,2,3Îã®Í≥Ñ Î∞ùÍ∏∞        
     always @(posedge clk or posedge reset_p) begin
         if(reset_p) begin
-            dec1=0;
-            dec10=0;
+            cnt_btn =0;
+        end
+        else if(btn_pedge) begin
+            cnt_btn= cnt_btn+1;    
+        end        
+    end        
+              
+    always@(posedge clk or posedge reset_p) begin
+        if(reset_p) begin
+            led_pwm_o =0;
         end
         else begin
-            if(load_enable)begin
-                dec1 = set_value1;
-                dec10 = 0;
-            end
-            else if(clk_time) begin
-                if(dec1 == 0) begin  //0∫∏¥Ÿ ¿€æ∆¡˙ ºˆ æ¯æÓ
-                     dec1 = 9;
-                     if (dec10 == 0) begin
-                        dec10 =5;
-                        dec_clk = 1; // cooktimer∏µ‚¿« ∫– 00ø°º≠  59√  â—¿ª ∂ß 1∫–±¿Ã∞‘ µø±‚»≠
-                     end
-                     else dec10 = dec10 - 1;
-                end
-                else dec1 = dec1 -1;
-            end
-            else dec_clk =0; // ø©±‚º≠ 0 «ÿº≠ øß¡ˆ ¿‚¿ª « ø‰ æ¯æÓ (one cycle pulse∂Ûº≠)
-        end
+            case(cnt_btn)
+                2'b00: begin led_pwm_o = 0; end
+                2'b01: begin led_pwm_o = led_pwm[0]; end
+                2'b10: begin led_pwm_o = led_pwm[1]; end
+                2'b11: begin led_pwm_o = 1; end
+            endcase
+        end    
     end
+    
 endmodule
-//////////////////////////////////////////////////
-module cook_timer_fan(   //«—π¯¥©∏£∏È Ω∫≈∏∆Æ, «—π¯¥©∏£∏È Ω∫≈È 1π¯πˆ∆∞ √ ¡ı∞°, 2π¯πˆ∆∞ ∫–¡ı∞°
+
+module dc_motor_speed( 
     input clk, reset_p,
-    input btn_nedge,
-    input btn_pedge,
-    output [16:0] value,
-    output reg [2:0]led_timer
-    );
-    wire clk_usec, clk_msec, clk_sec, clk_min;
-    wire [3:0] cur_msec, cur_sec1, cur_sec10, cur_min1, cur_min10;
-    reg cur_msec_e;
-    wire load_enable;
-    wire dec_clk, clk_start;
-    wire [15:0]  cur_time;
-   
-    clock_usec usec_clk(clk_start, reset_p, clk_usec);
-    clock_div_1000 msec_clk(clk_start, reset_p, clk_usec, clk_msec);
-    clock_div_1000 sec_clk(clk_start, reset_p, clk_msec, clk_sec);
+    input btn,
+    input motor_off,
+    input [11:0]distance,
+    output reg motor_pwm_o,
+    output reg [2:0]motor_led,
+    output reg motor_sw
+);
+    wire [1:0]motor_pwm;
+    reg [27:0] clk_div = 0;
+    always@(posedge clk) clk_div =clk_div+1;
     
-    reg timer_e;
-    reg [1:0] cnt;
-    reg [7:0] timer135; //[5:0]
-    wire timeout_e_pedge;
-    reg timeout_e;
-
-     assign clk_start = timer_e ? clk : 0;
-    
-     always @ (posedge clk or posedge reset_p) begin
+    pwm_100pc led0(.clk(clk), //1Îã®
+              .reset_p(reset_p),
+              .duty(25),
+              .pwm_freq(1_00),
+              .pwm_100pc(motor_pwm[0])
+              );
+              
+    pwm_100pc led1(.clk(clk), //2Îã®
+              .reset_p(reset_p),
+              .duty(45),
+              .pwm_freq(1_00),
+              .pwm_100pc(motor_pwm[1])
+              );
+    wire btn_pedge;        
+    button_cntr btn_cntr0(.clk(clk), .reset_p(reset_p), .btn(btn), .btn_pe(btn_pedge));         
+            
+    reg [1:0]cnt_btn; //0,1,2,3 -> Í∫ºÏßê, 1,2,3Îã®Í≥Ñ Î∞ùÍ∏∞        
+    always @(posedge clk or posedge reset_p) begin
         if(reset_p) begin
-            cnt = 0;
-            timer135 = 0;          
-            timer_e = 0;
-            cur_msec_e = 1;
+            cnt_btn =0;
         end
-        else if ( btn_nedge) timer_e = 1;
-        else if (btn_pedge) begin
-            timer_e = 0;
-            cnt = cnt + 1;
-         
-            case(cnt)
-                2'b00 : begin
-                    timer135 = 0;
-                    led_timer = 0;
-                    cur_msec_e = 1;
-               end
-                2'b01 : begin
-                    led_timer = 0;
-                    timer135 = 1;
-                    led_timer[0] = 1;
-                    cur_msec_e = 0;
-                end
-                2'b10 : begin
-                    led_timer = 0;
-                    timer135 = 3;
-                    led_timer[1] = 1;
-                    cur_msec_e = 0;
-                end
-                2'b11 : begin
-                    led_timer = 0;
-                    timer135 = 5;
-                    led_timer[2] = 1;
-                    cur_msec_e = 0;
-                end                
-                default  timer135 = 0 ;
-             endcase
-            end
-        else if (timeout_e_pedge) timer_e = 0;
-    end
-    
-    edge_detector_n ed1(.clk(clk), .reset_p(reset_p), .cp(timer_e), .p_edge(load_enable));
-    
-
-    loadable_downcounter_dec_60_fan cur_sec(.clk(clk), .reset_p(reset_p), .clk_time(clk_sec), .load_enable(load_enable),
-                .dec1(cur_sec1), .dec10(cur_sec10), .dec_clk(dec_clk));
-    loadable_downcounter_dec_60_fan cur_min(.clk(clk), .reset_p(reset_p), .clk_time(dec_clk), .load_enable(load_enable),
-                 .set_value1(timer135), .dec1(cur_min1), .dec10(0));
-    
-    always @ (posedge clk or posedge reset_p) begin
+        else if(btn_pedge) begin cnt_btn= cnt_btn+1; end
+        else if(motor_off) begin cnt_btn=0; end 
+        else if(distance>=11'h09) begin cnt_btn = 0; end
+    end        
+              
+    always@(posedge clk or posedge reset_p) begin
         if(reset_p) begin
-            timeout_e = 0;
+            motor_pwm_o =0;
+            motor_sw = 0;
         end
         else begin
-            if (timer_e && clk_msec && cur_time == 0) begin
-                timeout_e = 1;
-            end
-            else begin
-                timeout_e =0;
-            end
+            case(cnt_btn)
+                2'b00: begin motor_pwm_o = 0;               motor_sw =0;      motor_led=3'b000; end
+                2'b01: begin motor_pwm_o = motor_pwm[0];    motor_sw =1;      motor_led=3'b001; end
+                2'b10: begin motor_pwm_o = motor_pwm[1];    motor_sw =1;      motor_led=3'b010; end
+                2'b11: begin motor_pwm_o = 1;               motor_sw =1;      motor_led=3'b100; end
+            endcase
+        end    
+    end
+   
+endmodule
+
+
+module fan_timer(   //ÌÉÄÏù¥Î®∏
+    input clk, reset_p,
+    input btn_str,
+    input motor_sw,
+    output reg motor_off,
+    output [3:0]com,
+    output [7:0]seg_7,
+    output reg [2:0]timer_led
+    );
+    wire btn_str_pedge, btn_str_nedge, btn_sw;
+    button_cntr btn_cntr1(.clk(clk), .reset_p(reset_p), .btn(btn_sw), .btn_pe(btn_str_pedge), .btn_ne(btn_str_nedge));
+    
+    assign btn_sw = motor_sw ? btn_str : 0;
+    
+    wire btn_start, inc_sec, inc_min, alarm_off; //Î≤ÑÌäº 0Î≤à 1Î≤à 2Î≤à 3Î≤à
+    wire [3:0] set_sec1, set_sec10, set_min1, set_min10;
+    wire [3:0] cur_sec1, cur_sec10, cur_min1, cur_min10;
+    wire load_enable, dec_clk, clk_start; //clk_start : startÌñàÏùÑ ÎïåÎßå ÌÅ¥Îü≠Ïù¥ ÎÇòÏò§ÎèÑÎ°ù Ìï®
+    reg start_stop;
+    wire [16:0]cur_time,cur_time_1;
+    wire [15:0]set_time;
+    wire timeout_pedge;
+    reg time_out;
+    reg motor_e;
+    
+    assign clk_start = start_stop ?  clk : 0;
+    
+    clock_usec usec_clk(clk_start, reset_p, clk_usec);
+    clock_div_1000 msec_clk(clk_start,reset_p,clk_usec, clk_msec);
+    clock_div_1000 sec_clk(clk_start,reset_p,clk_msec, clk_sec);
+    
+    reg [1:0]setting_state;
+    reg [2:0]setting_time;
+    
+    always@(posedge clk or posedge reset_p) begin //1->3->5Î∂Ñ->ÌÉÄÏù¥Î®∏ Ìï¥Ï†ú
+        if(reset_p) begin
+            setting_time=0;
+            timer_led =0;
+        end
+        else begin
+            case(setting_state)
+                2'b00: begin setting_time=0; timer_led = 3'b000; motor_e = 1; end
+                2'b01: begin setting_time=1; timer_led = 3'b001; motor_e = 0; end
+                2'b10: begin setting_time=3; timer_led = 3'b010; motor_e = 0; end
+                2'b11: begin setting_time=5; timer_led = 3'b100; motor_e = 0; end
+            endcase
+         end
+    end
+    
+    always@(posedge clk or posedge reset_p) begin
+        if(reset_p) setting_state=0;
+        else if(btn_str_pedge) setting_state = setting_state + 1;
+        else if(timeout_pedge) setting_state = 2'b00;
+    end
+    
+    always @(posedge clk or posedge reset_p) begin
+        if(reset_p) time_out =0;
+        else begin
+            if(start_stop &&clk_msec && cur_time ==0) time_out = 1;
+            else  time_out = 0;
         end
     end
     
-    edge_detector_n ed_timeout_e(.clk(clk), .reset_p(reset_p), .cp(timeout_e),  .p_edge(timeout_e_pedge));
-   
-    assign cur_time = {cur_min10, cur_min1, cur_sec10, cur_sec1, cur_msec_e};
-    assign value = cur_time;
-    reg [16:0] clk_div =0;
-    always @(posedge clk) clk_div = clk_div +1;
+    edge_detector_n ed_timeout(.clk(clk), .reset_p(reset_p), .cp(time_out), .p_edge(timeout_pedge));
+    
+    always @ (posedge clk or posedge reset_p)begin
+         if(reset_p) begin
+             start_stop = 0;
+             motor_off = 0 ;
+         end
+         else begin
+         if(btn_str_pedge) start_stop = 0;
+         else if (btn_str_nedge) start_stop = 1; //start or stop
+         else if(timeout_pedge) begin
+             start_stop = 0;
+             motor_off = 1;
+         end
+         else motor_off = 0;
+         end
+     end
+     
+    edge_detector_n ed(.clk(clk), .reset_p(reset_p), .cp(start_stop), .p_edge(load_enable));
+    loadable_downcounter_dec_60 cur_sec(.clk(clk),
+                                            .reset_p(reset_p),
+                                            .clk_time(clk_sec),
+                                            .load_enable(load_enable),
+                                            .dec1(cur_sec1),
+                                            .dec10(cur_sec10),
+                                            .dec_clk(dec_clk));
+                                            
+    loadable_downcounter_dec_60 cur_min(.clk(clk),
+                                            .reset_p(reset_p),
+                                            .clk_time(dec_clk),
+                                            .load_enable(load_enable),
+                                            .set_value1(setting_time),
+                                            .dec1(cur_min1),
+                                            .dec10(0));
+    wire [16:0]value;
+    assign cur_time = {cur_min10, cur_min1, cur_sec10, cur_sec1, motor_e};
+    assign cur_time_1 = setting_time ? cur_time : 0;
+    assign value = start_stop ? cur_time_1 : 0;
+    
+    fnd_4digit_cntr fnd(.clk(clk), .reset_p(reset_p), .value(value[16:1]), .seg_7_ca(seg_7), .com(com));
+    
 endmodule
-///////////////////////////////////////////////////////////////////
 
+/////////////////UltraSonic Ï¥àÏùåÌååÏÑºÏÑú
+module ultra_sonic_prof_fan(
+    input clk, reset_p,
+    input echo, 
+    output reg trigger,
+    output reg [11:0] distance
+);
+    
+    parameter S_IDLE    = 3'b001;
+    parameter TRI_10US  = 3'b010;
+    parameter ECHO_STATE= 3'b100;
+    
+    parameter S_WAIT_PEDGE = 2'b01;
+    parameter S_WAIT_NEDGE = 2'b10;
+    
+    reg [21:0] count_usec;
+    wire clk_usec;
+    reg count_usec_e;
+    
+    clock_usec usec_clk(clk, reset_p, clk_usec);
+    
+    always @(negedge clk or posedge reset_p)begin
+        if(reset_p) count_usec = 0;
+        else begin
+            if(clk_usec && count_usec_e) count_usec = count_usec + 1;
+            else if(!count_usec_e) count_usec = 0;
+        end
+    end
+    wire echo_pedge, echo_nedge;
+    edge_detector_n ed(.clk(clk), .reset_p(reset_p), .cp(echo), .p_edge(echo_pedge), .n_edge(echo_nedge));
+    
+    reg [11:0] echo_time;
+    reg [3:0] state, next_state;
+    reg [1:0] read_state;
+    
+       reg cnt_e;
+       wire [11:0] cm;
+       sr04_div58 div58(clk, reset_p, clk_usec, cnt_e, cm);
+
+    always @(negedge clk or posedge reset_p)begin
+        if(reset_p) state = S_IDLE;
+        else state = next_state;
+    end
+    
+    always @(posedge clk or posedge reset_p)begin  
+        if(reset_p)begin
+            count_usec_e = 0;
+            next_state = S_IDLE;
+            trigger = 0;
+            read_state = S_WAIT_PEDGE;
+        end
+        else begin
+            case(state)
+                S_IDLE:begin
+                    if(count_usec < 22'd100_000)begin 
+                        count_usec_e = 1; 
+                    end
+                    else begin 
+                        next_state = TRI_10US;
+                        count_usec_e = 0; 
+                    end
+                end
+                TRI_10US:begin 
+                    if(count_usec <= 22'd10)begin 
+                        count_usec_e = 1;
+                        trigger = 1;
+                    end
+                    else begin
+                        count_usec_e = 0;
+                        trigger = 0;
+                        next_state = ECHO_STATE;
+                    end
+                end
+                ECHO_STATE:begin 
+                    case(read_state)
+                        S_WAIT_PEDGE:begin
+                            count_usec_e = 0;
+                            if(echo_pedge)begin
+                                read_state = S_WAIT_NEDGE;
+                                cnt_e = 1;  //Ï∂îÍ∞Ä
+                            end
+                        end
+                        S_WAIT_NEDGE:begin
+                            if(echo_nedge)begin       
+                                read_state = S_WAIT_PEDGE;
+                                count_usec_e = 0;                    
+                                distance = cm;  //Ï∂îÍ∞Ä
+                                    
+                                cnt_e =0;       //Ï∂îÍ∞Ä
+                                next_state = S_IDLE;
+                            end
+                            else begin
+                                count_usec_e = 1;
+                            end
+                        end
+                    endcase
+                end
+                default:next_state = S_IDLE;
+            endcase
+        end
+    end
+endmodule
+
+module servomotor(
+    input clk, reset_p,
+    input btn_str,
+    input motor_sw,
+    output sg90
+);
+    wire btn_pedge;
+    
+    button_cntr btn_cntr(.clk(clk), .reset_p(reset_p), .btn(btn_str), .btn_pe(btn_pedge));
+    
+    reg turn_on;
+
+    always @(posedge clk or posedge reset_p)begin
+        if(reset_p)turn_on = 0;
+        else if(btn_pedge)     begin turn_on = turn_on + 1; end
+        else if(motor_sw == 0) begin turn_on = 0; end
+    end
+    
+    reg [31:0] clk_div;
+    always @(posedge clk)clk_div = clk_div + 1;
+    
+    wire clk_div_pedge;
+    edge_detector_n ed0(.clk(clk), .reset_p(reset_p), .cp(clk_div[22]), .p_edge(clk_div_pedge));
+    
+    reg [8:0] duty;
+    reg up_down;
+   
+    always @(posedge clk or posedge reset_p)begin
+        if(reset_p)begin
+            duty = 14;
+            up_down = 1;
+        end
+        else if(motor_sw)begin 
+            if(turn_on)begin
+                if(clk_div_pedge)begin
+                    if(duty >= 70)up_down = 0;
+                    else if(duty <= 8)up_down = 1;
+                        
+                    if(up_down)duty = duty + 1;
+                    else duty = duty - 1;
+                end
+            end
+        end
+    end
+
+    pwm_512step servo0(.clk(clk), .reset_p(reset_p), .duty(duty), .pwm_freq(50), .pwm_512(sg90));
+
+endmodule
